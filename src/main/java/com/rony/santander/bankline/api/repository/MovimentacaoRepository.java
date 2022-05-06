2ddd2a259d4888b50f5caf94ed95369f90468892
Rony Marcos da Silva Alves <63080722+RonyMarcos97@users.noreply.github.com>
2022-05-05 23:13:03 -0300

    atualização

--- src/main/java/com/rony/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/rony/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -1,10 +1,14 @@
 package com.rony.santander.bankline.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rony.santander.bankline.api.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>{
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
 	
 
 }
